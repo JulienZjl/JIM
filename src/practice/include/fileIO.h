ef701a3d84239933f314caf106a729853ea9642e
JulianZh <julian.zjl@outlook.com>
2017-08-31 20:00:30 +0800

    update

--- src/practice/include/fileIO.h
+++ src/practice/include/fileIO.h
@@ -1,10 +1,20 @@
 /* fileIO.h */
 
-#ifdef __FILEIO_H__
+
+#ifndef __FILEIO_H__
 #define __FILEIO_H__
 
+int fgetc(FILE *fp);
+int fputc(int ch, FILE *fp);
+int getchar();
+int putchar(int ch);
 
+char *fgets(char *s, int n, FILE *fp);
+char *gets(char *s);
+int fputs(const char *s, FILE *fp);
+int puts(const char *s);
 
+void IO_copyFile();
 
 
 #endif
--- src/practice/src/fileIO.cpp
+++ src/practice/src/fileIO.cpp
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 #include "../include/fileIO.h"
 
-int main(int argc, char *argv[])
+#define BUF_SIZE 10
+
+
+int main1(int argc, char *argv[])
 {
 	FILE *fp;
 	int ch;
@@ -30,3 +33,41 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
+
+int main(int argc, char *argv[])
+{
+	printf("Copy file!!!\n");
+	
+	IO_copyFile();
+
+	return 0;
+}
+
+void IO_copyFile()
+{
+	FILE *fp1, *fp2;
+	char buf[BUF_SIZE];
+	char *str = NULL;
+
+	if ((fp1 = fopen("../hello", "r+")) == NULL)
+	{
+		printf("Open file '../a.txt' false!\n");
+		exit(1);
+	}
+
+	if ((fp2 = fopen("../hello1", "w+")) == NULL)
+	{
+		printf("Open file '../b.txt' false!\n");
+		exit(1);
+	}
+
+	while ((str = fgets(buf, BUF_SIZE, fp1)) != NULL)
+	{
+		fputs(str, fp2);
+	}
+
+	fclose(fp2);
+	fclose(fp1);
+}
+
